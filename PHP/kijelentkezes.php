dd194d4d61de5840d1e6a4f12d5313f938ee9a55
burjanbalazs <burjanb1@gmail.com>
2019-12-07 19:05:12 +0200

    removed unnecessary code

--- PHP/kijelentkezes.php
+++ PHP/kijelentkezes.php
@@ -1,7 +1,5 @@
 <?php
 session_start();
-$kapcsolodas=mysql_connect('localhost','root','');
-$adatbazis=mysql_select_db('users',$kapcsolodas);
 unset($_SESSION['felhasznalonev']);
 unset($_SESSION);
 session_destroy();
